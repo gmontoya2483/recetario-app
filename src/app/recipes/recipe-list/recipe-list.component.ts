fea3ed69cd24926909a9fb252d988e1e7b588d21
Gabriel Montoya <gabi.montoya@gmail.com>
2019-01-06 20:45:07 +0100

    Section: 3 - Basics - component structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.models';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test Recipe",
+      "This is simple a test",
+  "https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg"
+    ),
+    new Recipe(
+      "A test Recipe",
+      "This is simple a test",
+      "https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg"
+    )
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
